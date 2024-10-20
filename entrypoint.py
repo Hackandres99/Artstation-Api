802c5446833fe402935deaa987babda52f6afb60
Hackandres99 <andres19saavedra9castro9@gmail.com>
2024-10-20 03:16:35 -0500

    Adding WSGI server

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 from app.routes import app
+from waitress import serve
 
 load_dotenv()
 app.config.from_object(os.getenv('CONFIG_ENV'))
+if __name__ == "__main__":
+    serve(app, host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
